b8a9a5b2232b48b0a0b4fe649ee4bfc4651641e5
Igor Serafim <igorserafim.contato@gmail.com>
2023-10-26 19:14:25 -0300

    create header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import clsx from 'clsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={clsx(
+          inter.className,
+          'bg-zinc-950 text-zinc-50 antialiased',
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
